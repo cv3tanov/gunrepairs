a23d26b8ff95de1a495a43461ede30bb1f567e9e
cv3tanov <94202270+cv3tanov@users.noreply.github.com>
2024-08-09 20:49:03 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,4 +3,4 @@ Config = {}
 Config.model = 'ig_casey'
 Config.coords4 = vector4(143.022705, -575.4915, 22.070166, 331.856934)
 Config.coords = vec3(143.022537, -575.492798, 22.070166)
-Config.time = 1 -- in minutes repair finish
+Config.time = 1 -- 1 мин. Колко време ще поправя оръжието 
